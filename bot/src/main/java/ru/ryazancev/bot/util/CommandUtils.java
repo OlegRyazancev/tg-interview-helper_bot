59b9fbf2e6d9f75fbacbecf3264b705a5c9df3ec
Oleg Ryazancev <oleg.ryazancev1022@gmail.com>
2024-04-08 19:30:00 +0300

    Update command utils

--- bot/src/main/java/ru/ryazancev/bot/util/CommandUtils.java
+++ bot/src/main/java/ru/ryazancev/bot/util/CommandUtils.java
@@ -1,12 +1,11 @@
 package ru.ryazancev.bot.util;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
 import ru.ryazancev.bot.command.type.CommandDetails;
 import ru.ryazancev.bot.util.exception.UnknownCommandException;
-import ru.ryazancev.bot.util.message.impl.CommandDescription;
+import ru.ryazancev.bot.util.message.impl.CommandDescriptionProcessor;
 
 import java.util.Arrays;
 import java.util.List;
@@ -20,19 +19,19 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class CommandUtils {
 
-    private final CommandDescription commandDescription;
+    private final CommandDescriptionProcessor commandDescriptionProcessor;
 
     public <T extends Enum<T> & CommandDetails> List<BotCommand> createCommands(Class<T> enumClass) {
 
         return Arrays.stream(enumClass.getEnumConstants())
                 .map(val -> new BotCommand(
                         val.getCommandName(),
-                        commandDescription.getMessage(val.getDescription(), null, LocaleContextHolder.getLocale())
+                        commandDescriptionProcessor.getMessage(val.getDescription(), null)
                 ))
                 .collect(Collectors.toList());
     }
 
-    public static <T extends Enum<T> & CommandDetails> T fromText(String text, Class<T> enumClass) throws UnknownCommandException {
+    public static <T extends Enum<T> & CommandDetails> T fromTextToCommand(String text, Class<T> enumClass) throws UnknownCommandException {
 
         for (T command : enumClass.getEnumConstants()) {
             if (command.name().equalsIgnoreCase(text.substring(1))) {
