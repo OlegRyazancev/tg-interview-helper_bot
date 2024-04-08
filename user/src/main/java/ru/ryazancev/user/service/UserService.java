12109b05c3b2c50a1a7a6151e9f9cf27bdc17059
Oleg Ryazancev <oleg.ryazancev1022@gmail.com>
2024-04-08 19:31:07 +0300

    Add methods

--- user/src/main/java/ru/ryazancev/user/service/UserService.java
+++ user/src/main/java/ru/ryazancev/user/service/UserService.java
@@ -12,10 +12,13 @@ public interface UserService {
 
     List<User> findAll();
 
-    User findById(Long chatId);
+    User findById(Long id);
+
+    String resolveLocaleTag(Long chatId, String localeTag);
 
     void register(User user);
 
     String delete(Long chatId);
 
+    String getLocaleTagByChatId(Long chatId);
 }
--- user/src/main/java/ru/ryazancev/user/service/impl/UserServiceImpl.java
+++ user/src/main/java/ru/ryazancev/user/service/impl/UserServiceImpl.java
@@ -1,7 +1,9 @@
 package ru.ryazancev.user.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.ryazancev.user.model.User;
 import ru.ryazancev.user.repository.UserRepository;
 import ru.ryazancev.user.service.UserService;
@@ -14,8 +16,10 @@ import java.util.List;
  * @author Oleg Ryazancev
  */
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
@@ -36,20 +40,41 @@ public class UserServiceImpl implements UserService {
                                 .byChatId(String.valueOf(id)));
     }
 
+    @Transactional
+    @Override
+    public String resolveLocaleTag(Long chatId, String localeTag) {
+
+        userRepository.resolveLocaleTag(chatId, localeTag);
+        //todo: add messages
+        return "Locale resolved successfully!";
+    }
+
+    @Transactional
     @Override
     public void register(User user) {
 
-        if (userRepository.findById(user.getChatId()).isEmpty()) {
+        if (userRepository.findByChatId(user.getChatId()).isEmpty()) {
             user.setRegisteredAt(LocalDateTime.now());
             userRepository.save(user);
+        } else {
+            log.warn("User with same chat id: {} already exists",
+                    user.getChatId());
         }
     }
 
+    @Transactional
     @Override
     public String delete(Long id) {
 
         userRepository.deleteById(id);
 
         return String.format("User with id %s was successfully deleted", id);
+    }
+
+    @Override
+    public String getLocaleTagByChatId(Long chatId) {
+
+        return userRepository.findLocaleByChatId(chatId)
+                .orElse("No locale");
     }
 }
