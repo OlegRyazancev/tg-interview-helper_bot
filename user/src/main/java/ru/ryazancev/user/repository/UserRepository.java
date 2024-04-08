57d715715a43ca153d2f6c0e277d766d13ca2593
Oleg Ryazancev <oleg.ryazancev1022@gmail.com>
2024-04-08 19:31:24 +0300

    Add methods in native query

--- user/src/main/java/ru/ryazancev/user/repository/UserRepository.java
+++ user/src/main/java/ru/ryazancev/user/repository/UserRepository.java
@@ -1,8 +1,14 @@
 package ru.ryazancev.user.repository;
 
+import org.apache.kafka.common.quota.ClientQuotaAlteration;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.ryazancev.user.model.User;
+
+import java.util.Optional;
 
 /**
  * @author Oleg Ryazancev
@@ -10,4 +16,20 @@ import ru.ryazancev.user.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Modifying
+    @Query(value = """
+            insert into users_locales (chat_id, locale)
+            values (:chatId, :locale)
+            """, nativeQuery = true)
+    void resolveLocaleTag(@Param("chatId") Long chatId,
+                          @Param("locale") String locale);
+
+    @Query(value = """
+            select locale from users_locales l
+            where l.chat_id = :chatId
+            """, nativeQuery = true)
+    Optional<String> findLocaleByChatId(@Param("chatId") Long chatId);
+
+    Optional<User> findByChatId(Long chatId);
 }
