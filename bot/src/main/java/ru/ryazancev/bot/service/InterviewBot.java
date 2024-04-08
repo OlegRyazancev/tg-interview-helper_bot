dc22f69bf395d0e4a04a1185dd24b20c08cc56d1
Oleg Ryazancev <oleg.ryazancev1022@gmail.com>
2024-04-08 19:32:42 +0300

    Refactor InterviewBot class

--- bot/src/main/java/ru/ryazancev/bot/service/InterviewBot.java
+++ bot/src/main/java/ru/ryazancev/bot/service/InterviewBot.java
@@ -6,11 +6,14 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
 import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
+import ru.ryazancev.bot.command.Button;
 import ru.ryazancev.bot.command.creator.CommandCreator;
-import ru.ryazancev.bot.handler.CallbackHandler;
+import ru.ryazancev.bot.handler.impl.callback.EditMessageCallbackHandler;
+import ru.ryazancev.bot.handler.impl.callback.SendMessageCallbackHandler;
 import ru.ryazancev.bot.handler.TextHandler;
 
 import java.util.List;
@@ -27,16 +30,19 @@ public class InterviewBot extends TelegramLongPollingBot {
     private String name;
 
     private final TextHandler textHandler;
-    private final CallbackHandler callbackHandler;
+    private final SendMessageCallbackHandler sendMessageCallbackHandler;
+    private final EditMessageCallbackHandler editMessageCallbackHandler;
     private final CommandCreator commandCreator;
 
     public InterviewBot(@Value("${interview-bot.token}") String botToken,
                         TextHandler textHandler,
-                        CallbackHandler callbackHandler,
+                        SendMessageCallbackHandler sendMessageCallbackHandler,
+                        EditMessageCallbackHandler editMessageCallbackHandler,
                         CommandCreator commandCreator) {
         super(botToken);
         this.textHandler = textHandler;
-        this.callbackHandler = callbackHandler;
+        this.sendMessageCallbackHandler = sendMessageCallbackHandler;
+        this.editMessageCallbackHandler = editMessageCallbackHandler;
         this.commandCreator = commandCreator;
         setupCommands();
         log.info("23423");
@@ -45,12 +51,17 @@ public class InterviewBot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
 
-        SendMessage sendMessage;
+        Object sendMessage;
 
         if (update.hasMessage() && update.getMessage().hasText()) {
             sendMessage = textHandler.handleTextUpdate(update);
         } else if (update.hasCallbackQuery()) {
-            sendMessage = callbackHandler.handleCallback(update);
+
+            if (update.getCallbackQuery().getData().equals(Button.RUSSIAN_BUTTON.name()) || update.getCallbackQuery().getData().equals(Button.ENGLISH_BUTTON.name())) {
+                sendMessage = editMessageCallbackHandler.handleCallback(update);
+            } else {
+                sendMessage = sendMessageCallbackHandler.handleCallback(update);
+            }
         } else {
             sendMessage = SendMessage.builder()
                     .chatId(update.getMessage().getChatId())
@@ -76,10 +87,18 @@ public class InterviewBot extends TelegramLongPollingBot {
         }
     }
 
-    private void sendMessage(SendMessage sendMessage) {
+    private void sendMessage(Object sendMessage) {
         try {
-            execute(sendMessage);
-            log.info("message executed " + sendMessage.getChatId());
+
+            if (sendMessage instanceof SendMessage) {
+                execute((SendMessage) sendMessage);
+                log.info("message executed " + ((SendMessage) sendMessage).getChatId());
+            } else {
+                execute((EditMessageText) sendMessage);
+                log.info("message executed " + ((EditMessageText) sendMessage).getChatId());
+            }
+
+
         } catch (Exception e) {
             log.error("Exception " + e.getMessage());
         }
